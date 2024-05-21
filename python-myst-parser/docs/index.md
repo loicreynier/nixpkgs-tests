6b9c9e686f8c74b0fad320b5f4f305614099b3e9
Loïc Reynier <loic@loicreynier.fr>
2024-05-21 08:55:20 +0000

    chore: add MyST parser shell

---
+++ python-myst-parser/docs/index.md
@@ -0,0 +1,40 @@
+---
+myst:
+  enable_extensions: ["deflist", "colon_fence"]
+---
+
+# Welcome to quickmaths's documentation
+
+## MyST
+
+:::{admonition} Looking for the API documentation?
+:class: tip
+
+Look out [below](#header-label)
+:::
+
+This (extensive) documentation is written using MyST
+which provides additional Markdown syntaxes such as definition lists:
+
+```markdown
+Term 1
+: Definition
+
+Term 2
+: Definition
+```
+
+Term 1
+: Definition
+
+Term 2
+: Definition
+
+(header-label)=
+
+## API reference
+
+```{eval-rst}
+.. automodule:: add
+   :members:
+```
---
+++ python-myst-parser/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "MyST Parser test devshell";
+
+  outputs = {nixpkgs, ...}: let
+    supportedSystems = [
+      "x86_64-linux"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          just
+          (python3.withPackages (ps:
+            with ps; [
+              sphinx
+              sphinx-rtd-theme
+              myst-parser
+            ]))
+        ];
+      };
+    });
+  };
+
+  inputs = {
+    nixpkgs.url = "github:loicreynier/nixpkgs/feat-myst-parser-update";
+  };
+}
