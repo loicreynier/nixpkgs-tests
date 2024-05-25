8807741089ebce0882ff184c643000a3c910894a
Loïc Reynier <loic@loicreynier.fr>
2024-05-25 08:52:56 +0000

    chore: add Spriggit shell

---
+++ spriggit/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Spriggit test devshell";
+
+  outputs = {
+    nixpkgs,
+    nixpkgs-lor,
+    ...
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [
+          nixpkgs-lor.overlays.default
+        ];
+      });
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          just
+          spriggit
+        ];
+      };
+    });
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-lor.url = "github:loicreynier/nixpkgs-lor";
+  };
+}
